7bb6dcef8094707ab5ec85f92c20473615ce7924
Denis Chepil <den@chepil.ru>
2025-02-23 23:37:55 +0300

    fix send serial data to mqtt

--- serialreader/script.py
+++ serialreader/script.py
@@ -4,10 +4,23 @@ Dependencies:
 """
 
 import serial
+from paho.mqtt import client as mqtt_client
+import random
+import time
 
 # This is the default serial port
 PORT = '/dev/ttyACM0'
 SPEED = 115200
+
+broker = 'mosquitto'
+port = 1883
+topic = "gpsloc"
+client_id = f'serial-{random.randint(0, 1000)}'
+
+FIRST_RECONNECT_DELAY = 1
+RECONNECT_RATE = 2
+MAX_RECONNECT_COUNT = 120
+MAX_RECONNECT_DELAY = 60
 
 # You may need to further configure settings
 # See the pyserial documentation for more info
@@ -16,7 +29,56 @@ ser = serial.Serial(port=PORT,
                     baudrate=SPEED,
                     timeout=1)
 
+def on_disconnect(client, userdata, rc):
+    print("Disconnected with result code: %s", rc)
+    reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
+    while reconnect_count < MAX_RECONNECT_COUNT:
+        print("Reconnecting in %d seconds...", reconnect_delay)
+        time.sleep(reconnect_delay)
+
+        try:
+            client.reconnect()
+            print("Reconnected successfully!")
+            return
+        except Exception as err:
+            print("%s. Reconnect failed. Retrying...", err)
+
+        reconnect_delay *= RECONNECT_RATE
+        reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
+        reconnect_count += 1
+    print("Reconnect failed after %s attempts. Exiting...", reconnect_count)
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        # For paho-mqtt 2.0.0, you need to add the properties parameter.
+        # def on_connect(client, userdata, flags, rc, properties):
+        if rc != 0:
+            #print("Connected to MQTT Broker!")
+        #else:
+            print("Failed to connect, return code %d\n", rc)
+    # Set Connecting Client ID
+    client = mqtt_client.Client(mqtt_client.CallbackAPIVersion.VERSION1, client_id)
+
+    # For paho-mqtt 2.0.0, you need to set callback_api_version.
+    # client = mqtt_client.Client(client_id=client_id, callback_api_version=mqtt_client.CallbackAPIVersion.VERSION2)
+
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.connect(broker, port)
+    return client
+
+def publish(client, msg):
+    result = client.publish(topic, msg)
+    # result: [0, 1]
+    status = result[0]
+    if status != 0:
+        #print(f"Send `{msg}` to topic `{topic}`")
+    #else:
+        print(f"Failed to send message to topic {topic}")
+
 try:
+    client = connect_mqtt()
     while True:
         # Read raw data from the stream
         # Convert the binary string to a normal string
@@ -24,5 +86,8 @@ try:
         message = ser.readline().decode().rstrip()
         if len(message) > 0:
             print(f'{message}')
+            client.loop_start()
+            publish(client, message)
+            client.loop_stop()
 finally:
     ser.close()
